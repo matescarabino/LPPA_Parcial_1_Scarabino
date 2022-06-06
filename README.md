f4ffbab0fdfd041141bd3b94698b2dfcb0d96814
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-06-06 17:57:34 -0300

    Pruebas modal
    commiteo por que sigo en otra pc

---
+++ README.md
@@ -0,0 +1,16 @@
+<h2>Parcial 1 Scarabino - LENGUAJES DE PROGRAMACIÓN PARA LA ADMINISTRACIÓN</h2>
+
+<p>Alumno: Scarabino Mateo<br>
+Carrera: T1-09 INGENIERÍA EN SISTEMAS INFORMÁTICOS<br>
+Comisión: 4-A<br>
+Turno: Noche<br>
+Inicio: 04/04/2022<br>
+Fin: 23/07/2022<br>
+<br>
+Profesor: Frare Esteban
+</p>
+
+<h2>ENTREGA</h2>
+
+Ver pagina funcionando: <a href="https://matescarabino.github.io/LPPA_Parcial_1_Scarabino/" target="_blank">click aqui</a><br>
+
--- js/main.js
+++ js/main.js
@@ -88,10 +88,8 @@ window.onload = function () {
     }
 
 
-
-
-    //Validaciones on Submit --------------------------------------------------------------------
-    document.formulario.onsubmit = function () {
+//Validacion on Submit --------------------------------------------------------------------
+    document.formulario.onsubmit = function (event) {
 
         //Validacion Nombre --------------------------------------------------------------------
         let nombre = document.getElementById('nombreInput');
@@ -185,9 +183,37 @@ window.onload = function () {
 
         }
 
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementById("close");
+
+        modal.style.display = "block";
+        
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+            document.getElementById("formulario").submit();
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+            modal.style.display = "none";
+            document.getElementById("formulario").submit();
+            }
+        }
+
+        event.preventDefault();
     }
 
 
+  
+
+
+    
 };
 
 
