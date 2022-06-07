32502e3701f1258961d60085f0c4754a9a34af70
Mateo Scarabino <matescarabino@gmail.com>
2022-06-06 22:56:27 -0300

    Ultimas correcciones y entrega
    - Corrijo errores ortográficos
    - Agrego comentarios de como funciona el código
    - Cambio visualmente el Modal
    - Arreglo bugs visuales al validar campos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 window.onload = function () {
 
+    //Llamo a la función  que realiza las validaciones onFocus/onBlur
     validacionesOnFocus();
     
-
+    //Declaro la función  que realiza las validaciones onFocus/onBlur
     function validacionesOnFocus() {
-        //Validacion Nombre --------------------------------------------------------------------
-        let nombre = document.getElementById('nombreInput');
-        let name_format = /^[a-zA-Z]+$/;
+        //Validación Nombre --------------------------------------------------------------------
+        let nombre = document.getElementById('nombreInput'); // Me traigo a una variable el input ingresado por el usuario
+        let name_format = /^[a-zA-Z]+$/; //Formato regex que solo acepta letras (mayúsculas incluidas)
 
+        //Válido (cuando se pierde foco) si lo ingresado es correcto
         nombre.onblur = function () {
             if ((nombre.value.length < 3) || (!nombre.value.match(name_format))
             ) {
-                nombre.classList.add('invalid');
+                nombre.classList.add('invalid'); // Si no es correcto, agrego la clase "invalid", la cual cambia el color a rojo y muestro el mensaje de error
                 errorNombre.innerHTML = 'Ingrese un nombre no menor a 3 letras.'
+
             }
         };
+        //Cuando se tiene foco nuevamente remuevo la clase "invalid" y el mensaje de error, ya que asumo que lo está corrigiendo 
         nombre.onfocus = function () {
             if (nombre.classList.contains('invalid')) {
                 nombre.classList.remove('invalid');
-                errorNombre.innerHTML = "";
+                errorNombre.innerHTML = "&nbsp;";
             }
         };
 
-        //Validacion Apellido -------------------------------------------------------------------
+        //Validación Apellido -------------------------------------------------------------------
         let apellido = document.getElementById('apellidoInput');
         let surname_format = /^[a-zA-Z]+$/;
 
@@ -31,67 +35,62 @@ window.onload = function () {
             ) {
                 apellido.classList.add('invalid');
                 errorApellido.innerHTML = 'Ingrese un apellido no menor a 3 letras.'
+
             }
         };
         apellido.onfocus = function () {
             if (apellido.classList.contains('invalid')) {
                 apellido.classList.remove('invalid');
-                errorApellido.innerHTML = "";
+                errorApellido.innerHTML = "&nbsp;";
             }
         };
 
-        // Validacion Mail ------------------------------------------------------------------------
+        // Validación Mail ------------------------------------------------------------------------
         let mail = document.getElementById('emailInput');
         let mail_format = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 
         mail.onblur = function () {
             if (!mail.value.match(mail_format)) {
                 mail.classList.add('invalid');
-                errorMail.innerHTML = 'Ingrese un email valido.'
+                errorMail.innerHTML = 'Ingrese un email válido.'
+
             }
         };
         mail.onfocus = function () {
             if (mail.classList.contains('invalid')) {
                 mail.classList.remove('invalid');
-                errorMail.innerHTML = "";
+                errorMail.innerHTML = "&nbsp;";
             }
         };
 
 
-        //Validacion Edad ----------------------------------------------------------
+        //Validación Edad ----------------------------------------------------------
         let edad = document.getElementById('edadInput');
         let age_format = /^-?\d*(\.\d+)?$/;
 
         edad.onblur = function () {
             if (!(edad.value.match(age_format)) || (edad.value < 1) || (edad.value > 99) || (edad.value == "")) {
                 edad.classList.add('invalid');
-                errorEdad.innerHTML = 'Ingrese un numero entero mayor a 0 y menor a 100.'
+                errorEdad.innerHTML = 'Ingrese un número entero entre 0 y 100.'
+
             }
         };
         edad.onfocus = function () {
             if (edad.classList.contains('invalid')) {
                 edad.classList.remove('invalid');
-                errorEdad.innerHTML = "";
+                errorEdad.innerHTML = "&nbsp;";
             }
         };
-
-        //Validacion Sexo ----------------------------------------------------------
-        // let y = document.getElementById('sexo');
-        // console.log(y)
-        // y.addEventListener('change', function(){
-        //     document.getElementById('sexoInput').classList.remove('invalid');
-        //     errorSexo.innerHTML = "";
-        // })
-
-
 
     }
 
 
-//Validacion on Submit --------------------------------------------------------------------
+//Validación on Submit --------------------------------------------------------------------
+    //Esta funcion se ejecuta cuando el usuario hace click en el boton "Enviar", se encarga de validar que todos los campos sean correctos.
+    //Si alguno de los campos es incorrecto hace un return, no sigue avanzando y remarca el error.
+    //Al llegar al final, es decir, cuando todas las validaciones son correctas llama a la funcion mostrarModal();
     document.formulario.onsubmit = function (event) {
-
-        //Validacion Nombre --------------------------------------------------------------------
+        //Validación Nombre --------------------------------------------------------------------
         let nombre = document.getElementById('nombreInput');
         let name_format = /^[a-zA-Z]+$/;
 
@@ -101,7 +100,7 @@ window.onload = function () {
             return false; //se utiliza para abortar la funcion
         }
 
-        //Validacion Apellido --------------------------------------------------------------------
+        //Validación Apellido --------------------------------------------------------------------
         let apellido = document.getElementById('apellidoInput');
         let surname_format = /^[a-zA-Z]+$/;
 
@@ -111,50 +110,49 @@ window.onload = function () {
             return false;
         }
 
-        //Validacion Email --------------------------------------------------------------------
+        //Validación Email --------------------------------------------------------------------
         let mail = document.getElementById('emailInput');
         let mail_format = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 
         if (!mail.value.match(mail_format) || (mail.value == "")) {
             mail.classList.add('invalid');
-            errorMail.innerHTML = 'Ingrese un email valido.';
+            errorMail.innerHTML = 'Ingrese un email válido.';
             return false;
         }
 
-        //Validacion Edad ----------------------------------------------------------
+        //Validación Edad ----------------------------------------------------------
         let edad = document.getElementById('edadInput');
         let age_format = /^-?\d*(\.\d+)?$/;
 
         if (!(edad.value.match(age_format)) || (edad.value < 1) || (edad.value > 99) || (edad.value == "")
         ) {
             edad.classList.add('invalid');
-            errorEdad.innerHTML = 'Ingrese un numero entero mayor a 0 y menor a 100.';
+            errorEdad.innerHTML = 'Ingrese un número entero mayor a 0 y menor a 100.';
             return false;
         }
 
-        //Validacion sexo ----------------------------------------------------------
+        //Validación sexo ----------------------------------------------------------
         if (formulario.sexo[0].checked == false && formulario.sexo[1].checked == false && formulario.sexo[2].checked == false) {
             document.getElementById('sexoInput').classList.add('invalid');
-            errorSexo.innerHTML = 'Seleccione una opcion.';
+            errorSexo.innerHTML = 'Seleccione una opción.';
             return false;
         } 
         document.getElementById('sexoInput').classList.remove('invalid');
         errorSexo.innerHTML = "";
         
 
-        //Validacion interes ----------------------------------------------------------
-
-        if (document.querySelector('.card__campo__input_checkbox:checked') == null) {
+        //Validación interes ----------------------------------------------------------
+        if (document.querySelector('.card__campo__input_checkbox:checked') == null) {//Me traigo todos los elementos de checkbox, ya que, con que uno este marcado (distinto de null) estaría correcta la validación
             document.getElementById('contenedorTemas').classList.add('invalid');
-            errorTemas.innerHTML = 'Seleccione por lo menos una opcion.';
+            errorTemas.innerHTML = 'Seleccione por lo menos una opción.';
             return false;
         }
         document.getElementById('contenedorTemas').classList.remove('invalid');
         errorTemas.innerHTML = "";
         
 
-        //Validacion Pais ----------------------------------------------------------
-        if (document.getElementById('paisInput').value == "") {
+        //Validación Pais ----------------------------------------------------------
+        if (document.getElementById('paisInput').value == "") { //Compruebo que el Input sea diferente al "país" (-- Elige un País --) que deje por defecto 
             document.getElementById('paisInput').classList.add('invalid');
             errorPais.innerHTML = 'Seleccione un país.';
             return false;
@@ -162,34 +160,36 @@ window.onload = function () {
         document.getElementById('paisInput').classList.remove('invalid');
         errorSexo.innerHTML = "";
 
-    
+        //Llamo a la función mostrarModal()
+        mostarModal();
 
+        //Al poner esta línea impido que se envie el formulario, y así que se cierre/refresque el Modal (a su vez el modal tiene el código necesario para cerrar y finalizar el submit)
+        event.preventDefault();
+    }
+
+    //Declaro la función mostrarModal()
+    function mostarModal() {
         // Ejecuto modal -----------------------------------------------------------
         let modal = document.getElementById("modalRegistro");
         let span = document.getElementById("close");
 
         // Lo hago visible
         modal.style.display = "block";
-        
-        // Si clickea el "boton" de aceptar escondo el modal
-        span.onclick = function() {
+
+        // Si clickea el "botón" de aceptar escondo el modal
+        span.onclick = function () {
             modal.style.display = "none";
             document.getElementById("formulario").submit();
         }
-        
+
         // Si clickea fuera del modal, lo escondo
-        window.onclick = function(event) {
+        window.onclick = function (event) {
             if (event.target == modal) {
-            modal.style.display = "none";
-            document.getElementById("formulario").submit();
+                modal.style.display = "none";
+                document.getElementById("formulario").submit();
             }
         }
-
-        event.preventDefault();
     }
-
-
-  
 
 
     
